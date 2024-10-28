c4033e8f90d3afc42c4f2b13aff62dfbce28e7c0
admin <admin@DESKTOP-Q6OG5UF>
2024-10-28 07:00:23 +0530

    second commit

---
+++ C2TC_BAVADHARANI/src/com/tns/ifet/day2/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day2;
+
+public class DoWhileDemo {
+	public static void main(String[] args)
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
---
+++ C2TC_BAVADHARANI/src/com/tns/ifet/day2/ForLoopExample.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day2;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=1000; i++)
+		{
+			System.out.println("Value of i:");
+			System.out.println(i);
+		}
+	}
+
+}

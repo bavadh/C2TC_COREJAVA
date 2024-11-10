0e13ad9f9e74f9e509f7a5b4855d35a686c863aa
admin <admin@DESKTOP-Q6OG5UF>
2024-11-10 12:12:32 +0530

    fifth commit

---
+++ C2TC_BAVADHARANI/src/com/tnsif/dayseven/overriding/covariant/ICICI.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayseven.overriding.covariant;
+
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
---
+++ C2TC_BAVADHARANI/src/com/tnsif/dayseven/overriding/covariant/RBI.java
@@ -0,0 +1,8 @@
+package com.tnsif.dayseven.overriding.covariant;
+
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
